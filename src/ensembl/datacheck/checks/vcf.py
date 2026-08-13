a8017bc9803390b6409d79a1407dcda767cbe3ce
Manuel Luypaert <luypaert@ebi.ac.uk>
2026-08-13 14:34:21 +0100

    feat: additional generic VCF header checks

--- src/ensembl/datacheck/checks/vcf.py
+++ src/ensembl/datacheck/checks/vcf.py
@@ -21,9 +21,11 @@ This module performs generic vcf checks.
 Checks performed:
 1. check_exist: Asserts that the target vcf path exists.
 2. check_is_gz_txt_file: Asserts that the target file is a gzipped text file.
-3. check_format: Asserts that the target file is readable as vcf.
+3. check_has_index_file: Asserts that an accompanying index file can be found.
+4. check_format: Asserts that the target file is readable as vcf.
 """
 
+from glob import glob
 from pathlib import Path
 
 from cyvcf2 import VCF
@@ -58,6 +60,22 @@ def check_is_gz_txt_file(target_file: str | Path):
     assert is_gz_text_file(target_file), "The file is not identified as a gzipped text file."
 
 
+def check_has_index_file(target_file: str | Path):
+    """
+    Check that an accompanying index file can be found.
+
+    Args:
+        target_file: The path to the file.
+
+    Raises:
+        AssertionError: If the file is not identified as a gzipped text file.
+    """
+
+    index_suffix = '.t[bs]i'
+    index_path = str(target_file) + index_suffix
+    assert len(glob(index_path)) == 1, f"No or more than one index file found for target file {target_file}."
+
+
 def check_format(target_file: str | Path):
     """
     Check that the target file is readable as vcf.
@@ -82,3 +100,30 @@ def check_format(target_file: str | Path):
     finally:
         if reader is not None:
             reader.close()
+
+
+def check_header(target_file: str | Path):
+    """
+    Check that the VCF header has all fields required for bcftools processing.
+
+    Args:
+        target_file: The path to the file.
+
+    Raises:
+        AssertionError: If the file is not identified as a gzipped text file.
+    """
+
+    reader = vcf_reader(target_file)
+
+    assert reader.get_header_type('fileformat') is not None, "The fileformat field is missing from the VCF header."
+
+    column_header_line = str(reader.raw_header.rstrip('\n').split('\n')[-1])
+    assert column_header_line.startswith('#'), "Column header line does not start with '#'."
+
+    columns = column_header_line[1:].split('\t')
+
+    assert columns[0] == 'CHROM', "Column header 'CHROM' not found at expected position."
+    assert columns[1] == 'POS', "Column header 'POS' not found at expected position."
+    assert columns[2] == 'ID', "Column header 'ID' not found at expected position."
+    assert columns[3] == 'REF', "Column header 'REF' not found at expected position."
+    assert columns[4] == 'ALT', "Column header 'ALT' not found at expected position."
